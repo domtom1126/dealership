3823eb42437230bb00022e041219e06cd4c4746a
domtom1126 <domtom1126@gmail.com>
2020-09-14 16:50:27 -0700

    When all cars displayed the list now shows as a table instead of just jumbled together

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, redirect, request, flash, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
-from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = '12345'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////.\vehicle_entry.db'
 db = SQLAlchemy(app)
 
-# MAKES TABLE
+app.secret_key = 'this is secret'
 class Vehicle(db.Model):
     make = db.Column(db.String(20), primary_key=True)
     model = db.Column(db.String(20), nullable=False)
@@ -16,41 +14,47 @@ class Vehicle(db.Model):
     color = db.Column(db.String(20), nullable=False)
     price = db.Column(db.Integer())
 
-class View_Past_Punches(db.Model):
-    emp_id = db.Column(db.Integer(), primary_key=True, nullable=True)
-    date = db.Column(db.String(40), nullable=False)
-
-#con = sql.connect('./vehicle_entry.db')
-#con.execute('CREATE TABLE IF NOT EXISTS view_past_punches(emp_id INTEGER PRIMARY KEY, date TEXT)')
-#con = sql.connect('./vehicle_entry.db')
-#con.execute('CREATE TABLE IF NOT EXISTS vehicle_entry(make TEXT PRIMARY KEY, model TEXT, year INTEGER, color TEXT, price INTEGER)')
+con = sql.connect('./vehicle_entry.db')
+con.execute('CREATE TABLE IF NOT EXISTS userbase(username TEXT PRIMARY KEY, password TEXT)')
 
 
-
-@app.route('/login')
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['new_username'] != 'root' or request.form['new_password'] != 'toor':
+            error = 'Wrong user or pass'
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', error=error)
 
-    return render_template('login.html')
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        new_username = request.form['new_username']
+        new_password = request.form['new_password']
+        
+        con = sql.connect('vehicle_entry.db')
+        c = con.cursor()
 
-@app.route('/clock_in', methods=['POST'])
-def clock_in(emp_id=None, date=None):
-    emp_id = request.form['emp_ID']
-    #Check emp_id db for correct id
-    
-    d = datetime.now()
+        c.execute('INSERT INTO userbase (new_username, new_password) VALUES (?,?)', (new_username, new_password))
 
-    con = sql.connect('vehicle_entry.db')
-    c = con.cursor()
-    # The ID needs to be UNIQUE, off for testing
-    c.execute("INSERT INTO view_past_punches (emp_id, date) VALUES (?, ?)", (emp_id, d,))
-    con.commit()
-    con.close()
-    flash('You are clocked in at')
-    return redirect(url_for('login'))
+        con.commit()
+        con.close()
+        
+        return redirect(url_for('dashboard', new_username = new_username, new_password =new_password))
+    else:
+        return render_template('register.html')
+        
+    return render_template('register.html')
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+@app.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/add_car')
+def add_car():
+    return render_template('add_car.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -69,17 +73,18 @@ def vehicle_entry(make=None, model=None, year=None, color=None, price=None):
     con.commit()
     con.close()
     print(make, model)
-    return (render_template('entry.html', make=make, model=model, year=year, color=color, price=price))
+    return (render_template('entry.html', model=model, make=make, year=year, color=color, price=price))
 
-@app.route('/view_cars', methods=['POST','GET'])
+
+@app.route('/view_cars', methods=['POST', 'GET'])
 def view_cars():
     con = sql.connect('vehicle_entry.db')
     c = con.cursor()
     c.execute("SELECT * FROM vehicle_entry")
     display = c.fetchall()
+
     return render_template('view_cars.html', display=display)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
